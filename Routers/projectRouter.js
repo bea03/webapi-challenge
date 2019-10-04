038045656902ab35a89b6104381a4beb6376aeb4
Nicole Beamer <tulip@Nicoles-iMac.home.sagemod>
2019-10-04 10:35:25 -0600

    Project Router done, tested in insomnia, working

--- Routers/projectRouter.js
+++ Routers/projectRouter.js
@@ -16,7 +16,7 @@ router.get('/:id', (req, res) => {
             res.status(200).json(project);
         })
         .catch(err => {
-            res.status(500).json({ error: "Projects could not be retrieved" });
+            res.status(500).json({ error: "Project could not be retrieved" });
         });
 });
 
@@ -31,6 +31,32 @@ router.post('/', validateProject, (req, res) => {
         });
 });
 
+//working
+router.delete('/:id', validateProjectID, (req, res) => {
+    const id = req.params.id;
+    projectModel.remove(id)
+        .then(value => {
+            res.status(200).json(value);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Could not be deleted from database"});
+        });
+});
+
+//working
+router.put('/:id', validateProjectID, validateProject, (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    projectModel.update(id, changes)
+        .then(project => {
+            res.status(200).json(project);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Unable to update project" });
+        });
+});
+
+//working
 function validateProject(req, res, next) {
     const projectBody = req.body;
     if(!projectBody.name || !projectBody.description || !projectBody) {
@@ -38,6 +64,22 @@ function validateProject(req, res, next) {
     } else {
         next();
     }
+}
+
+//working
+function validateProjectID(req, res, next) {
+    const id = req.params.id;
+    projectModel.get(id)
+        .then(project => {
+            if(!project){
+                res.status(404).json({ message: "ID not found" });
+            } else {
+                next();
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Project could not be retrieved" });
+        });
 }
 
 module.exports = router;
